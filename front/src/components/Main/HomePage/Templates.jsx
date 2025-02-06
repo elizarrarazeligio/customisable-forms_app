915a97cfa9c405591ed7a3a4a8a0f7a9321be00f
Eligio Elizarraraz <eligio2008@live.com.mx>
2025-02-06 09:50:55 -0600

    feat: added vercel json file for access to react-router-dom

--- front/src/components/Main/HomePage/Templates.jsx
+++ front/src/components/Main/HomePage/Templates.jsx
@@ -14,7 +14,7 @@ function Templates() {
           <Col className="col-7 fs-5 fw-medium">Initiate a new form</Col>
           <Col className="col-5"></Col>
         </Row>
-        <Row className="col-md-10 col-12 pb-1 mx-auto">
+        <Row className="col-md-10 col-12 pb-2 mx-auto">
           <Col className="col-6 col-md-4 col-xl-3 justify-content-center px-2 py-1 rounded">
             <img
               src={prueba}
@@ -23,39 +23,6 @@ function Templates() {
               style={{ height: 180 }}
             />
             <p className="badge text-dark fw-semibold fs-6 text-bg-light w-100 m-0 pt-2 text-truncate">
-              Formulario en blanco
-            </p>
-          </Col>
-          <Col className="col-6 col-md-4 col-xl-3 justify-content-center px-2 py-1 rounded">
-            <img
-              src={prueba}
-              alt="template"
-              className="object-fit-cover border rounded w-100 col-12"
-              style={{ height: 180 }}
-            />
-            <p className="badge text-dark fw-semibold fs-6 text-bg-light w-100 m-0 pt-2">
-              Formulario en blanco
-            </p>
-          </Col>
-          <Col className="col-6 col-md-4 col-xl-3 justify-content-center px-2 py-1 rounded">
-            <img
-              src={prueba}
-              alt="template"
-              className="object-fit-cover border rounded w-100 col-12"
-              style={{ height: 180 }}
-            />
-            <p className="badge text-dark fw-semibold fs-6 text-bg-light w-100 m-0 pt-2">
-              Formulario en blanco
-            </p>
-          </Col>
-          <Col className="col-6 col-md-4 col-xl-3 justify-content-center px-2 py-1 rounded">
-            <img
-              src={prueba}
-              alt="template"
-              className="object-fit-cover border rounded w-100 col-12"
-              style={{ height: 180 }}
-            />
-            <p className="badge text-dark fw-semibold fs-6 text-bg-light w-100 m-0 pt-2">
               Formulario en blanco
             </p>
           </Col>
