62b2c737fe7851d997ef991e44ded6baebafa3ba
Eligio Elizarraraz <eligio2008@live.com.mx>
2025-02-22 22:56:42 -0600

    feat: added chart js library to render bar chart for checkboxes questions

--- front/src/components/Main/HomePage/TemplateDashboard/TemplateDashboardTabs/Summary/Checkboxes.jsx
+++ front/src/components/Main/HomePage/TemplateDashboard/TemplateDashboardTabs/Summary/Checkboxes.jsx
@@ -1,11 +1,51 @@
-import Row from "react-bootstrap/esm/Row";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { Bar } from "react-chartjs-2";
 import { useState } from "react";
+Chart.register(CategoryScale);
 
 function Checkboxes({ answers }) {
+  const [chartData, setChartData] = useState({
+    labels: answers.map((answer) => answer.option),
+    datasets: [
+      {
+        label: "Votes",
+        data: answers.map((answer) => answer.count),
+        backgroundColor: [
+          "rgba(175, 172, 168, 0.83)",
+          "rgba(95, 92, 92, 0.74)",
+          "rgba(60, 61, 60, 0.85)",
+          "rgba(255, 255, 255, 0.85)",
+          "rgba(214, 212, 214, 0.85)",
+        ],
+        borderColor: "black",
+        borderWidth: 1,
+      },
+    ],
+  });
+
   return (
-    <>
-      <Row>Checkboxes</Row>
-    </>
+    <div className="col-12 col-md-10 col-lg-8 d-flex flex-column align-items-center">
+      <Bar
+        data={chartData}
+        options={{
+          responsive: true,
+          plugins: {
+            title: {
+              display: false,
+            },
+            legend: {
+              display: false,
+            },
+          },
+          scales: {
+            x: {
+              display: false,
+            },
+          },
+        }}
+      />
+    </div>
   );
 }
 
