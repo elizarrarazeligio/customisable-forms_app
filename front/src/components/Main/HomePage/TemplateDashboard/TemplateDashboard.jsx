a08cda82e2c63f9d3cb417c7be8e22c351d6456c
Eligio Elizarraraz <eligio2008@live.com.mx>
2025-02-23 12:33:48 -0600

    fix: tabs change on template dashboard now functioning correctly, as the tab keeps the track of the current page it is

--- front/src/components/Main/HomePage/TemplateDashboard/TemplateDashboard.jsx
+++ front/src/components/Main/HomePage/TemplateDashboard/TemplateDashboard.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import Container from "react-bootstrap/esm/Container";
 import Nav from "react-bootstrap/esm/Nav";
-import { useParams, Link, Outlet, useOutletContext } from "react-router-dom";
+import {
+  useParams,
+  Link,
+  Outlet,
+  useOutletContext,
+  useLocation,
+} from "react-router-dom";
 
 function TemplateDashboard() {
   const { templateHash } = useParams();
+  const location = useLocation();
+
   const [template] = useOutletContext();
+  const [defaultTab, setDefaultTab] = useState(
+    location.pathname.split("/")[2] || "general"
+  );
 
   return (
     <Container className="col-lg-10 col-12 flex-column mx-auto p-0">
       <Nav
         className="col-lg-10 col-11 mx-auto d-flex justify-content-center border-0 nav-tabs"
-        defaultActiveKey="general"
+        defaultActiveKey={defaultTab}
       >
         <Nav.Item>
           <Nav.Link as="div" eventKey="general" className="m-0 p-0">
