e355898291bd8d5de54f3304769c3ca95780105e
Eligio Elizarraraz <eligio2008@live.com.mx>
2025-02-24 19:07:01 -0600

    feat: added commentApi controller to bring comments data from backend to frontend

--- front/src/utils/commentApi.js
+++ front/src/utils/commentApi.js
@@ -4,7 +4,37 @@ class CommentApi {
     this._headers = headers;
   }
 
-  getTemplateComments(template_id) {}
+  getTemplateComments(template_id) {
+    return fetch(`${this._baseUrl}/template/${template_id}`).then((res) => {
+      if (res.ok) return res.json();
+      return Promise.reject(res.json());
+    });
+  }
+
+  addComment(template_id, user_id, description) {
+    return fetch(`${this._baseUrl}/template/${template_id}/add`, {
+      method: "POST",
+      headers: this._headers,
+      credentials: "include",
+      body: JSON.stringify({
+        user_id,
+        description,
+      }),
+    }).then((res) => {
+      if (res.ok) return res.json();
+      return Promise.reject(res.json());
+    });
+  }
+
+  deleteComment(comment_id) {
+    return fetch(`${this._baseUrl}/${comment_id}/delete`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then((res) => {
+      if (res.ok) return res.json();
+      return Promise.reject(res.json());
+    });
+  }
 }
 
 const commentApi = new CommentApi({
